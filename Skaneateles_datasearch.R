55216f5199b5659721233eb5b802ca678074a37e
Andrews518 <davidandrews7@hotmail.com>
2021-05-28 15:01:40 -0400

    update to use readNWISqw function

--- Skaneateles_datasearch.R
+++ Skaneateles_datasearch.R
@@ -1,10 +1,11 @@
 # install.packages('dataRetrieval')
 #install.packages('maps')
 library(dataRetrieval)
-library(maps)
+#library(maps)
 library(tidyverse)
+library(openxlsx)
 
-# Search for monitoring sites using a bounding box ----
+# A) Search for monitoring sites using a bounding box ----
   # bounding box must be in order: West Lon, South Lat, East Lon, North Lat
 sites <- whatNWISsites(bBox=c(-76.502778,42.716667,-76.166667,43.03335)) %>% 
   # Only pull data for lake and stream sites
@@ -13,56 +14,86 @@ sites <- whatNWISsites(bBox=c(-76.502778,42.716667,-76.166667,43.03335)) %>%
   # Take a look at query result
 head(sites)
 
-# Filter results to only site types of interest ----
   # Look at what type of sites remain
   # List of site_tp_cd: https://maps.waterdata.usgs.gov/mapper/help/sitetype.html
   unique(sites$site_tp_cd)
-  # . Decide which sites to keep and filter ----
-  # Pull out a list of site codes
+#   . Pull out a list of site codes from A ----
   sitnums <- unique(sites$site_no)
-#unique(sites$station_nm[sites$site_tp_cd=='LK'|sites$site_tp_cd=='ST'])
   
-# Look at the available data for all sites within bounding box ----
+# B) Look at the available data from sites found in section A (not a data download) ----
   # use "whatNWISdata()" to see what data is available for selected site numbers
-  try <- whatNWISdata(siteNumber = sitnums) %>% 
+  data_peek <- whatNWISdata(siteNumber = sitnums) %>% 
+    # Apply data filters
     filter(begin_date>='2000-01-01')
-  # . Create an object of stations not explicitly labelled "Skaneateles" ----
-  nolabel <- unique(try$station_nm[str_detect(try$station_nm, 
+#   . Examine data from B to see if they are from sites of interest ----
+  # Create an object of stations not explicitly labelled "Skaneateles" 
+  nolabel <- unique(data_peek$station_nm[str_detect(data_peek$station_nm, 
+                    # str_detect looks for a character string in a vector
+                    # the .* tells the function to look through the whole string
                                               'SKANEATELES .*', 
+                    # negate = T returns values without the character string that was specified
                                               negate = T)])
   
   # Get a list of lat and long for outlier sites to check in Google Earth
+    # This prints to the console and lat and long have to be manually entered into 
+    # Google Earth to see where the sites fall unless the name tells us that it should
+    # be kept/removed
   for (i in 1:NROW(nolabel)) {
-   x <- try[try$station_nm==nolabel[i],]
+   x <- data_peek[data_peek$station_nm==nolabel[i],]
    print(unique(x$station_nm))
    print(c(i,
            unique(c(x$dec_lat_va,x$dec_long_va))))
   }
 
   # Filter out sites determined to not be pertinent to study
-  # First make list of outlier sites to exclude
-  badsites <- nolabel[c(1,3:21,23,24)]
-  junk <- try %>% 
+  # First make list of outlier sites to EXCLUDE
+  badsites <- nolabel[c(1,3:21,23,24)] # These are sites to remove
+#   . Create object with info about data available from sites of interest ----
+  skandat <- data_peek %>% 
+    # only keep rows that are pertinent (station names NOT in `badsites` object)
     filter(!station_nm %in% badsites)
   
-  stream_dat <- junk %>% 
+# C) Create object containing parameter names to match with codes ----
+#   . Create a dataframe containing the parameter names for unique parameter codes ----
+  paramCds <- parameterCdFile
+  names(paramCds)[1] <- "parm_cd"
+  
+# D) Separate stream and Lake sites ----
+#   . Pull out stream sites ----
+  stream_dat <- skandat %>% 
     filter(site_tp_cd=='ST')
+  
+  stream_dat <-  merge(stream_dat, paramCds, by = "parm_cd", all.x = TRUE)
   
   strm_sites <- unique(stream_dat$site_no)
   
-  lake_dat <- junk %>% 
+#   . Pull out lake sites
+  lake_dat <- skandat %>% 
     filter(site_tp_cd=='LK')
   
+  lake_dat <-  merge(lake_dat, paramCds, by = "parm_cd", all.x = TRUE)
+
   lak_sites <- unique(lake_dat$site_no)
   
-# Pull data for selected sites ----
-stream_dat <- readNWISdata(siteNumber = strm_sites,
+# E) Data download ----
+#   . Pull data for selected sites ----
+final_strmdat <- readNWISdata(siteNumber = strm_sites,
                            startDate = '2000-01-01')
   
-stream_dat <- renameNWISColumns(stream_dat) 
+  final_strmdat <- renameNWISColumns(final_strmdat) 
+  
+  # write to file----
+  # write.xlsx(final_strmdat, file = 'USGS_Skan_streams.xlsx')
 
-lake_dat <- readNWISdata(siteNumber = lak_sites,
+final_lakedat <- readNWISdata(siteNumber = lak_sites,
                          startDate = '2000-01-01')
 
-  lake_dat <- renameNWISColumns(lake_dat)
+final_lakedat <- renameNWISColumns(final_lakedat)
   
+  # write to file----
+  # write.xlsx(final_lakedat, file = 'USGS_Skan_lake.xlsx')
+
+lakesiteinfo <- readNWISsite(lak_sites)
+
+write.xlsx(lakesiteinfo, file = "USGS_lakesite_info.xlsx")
+
